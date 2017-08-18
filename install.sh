ebb234d173f9bb6a8768a6b08d3070781feaeb09
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-08-17 20:12:23 -0500

    automated splunk startup

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ sudo yum -y install java
 curl https://download.splunk.com/products/splunk/releases/6.6.2/linux/splunk-6.6.2-4b804538c686-linux-2.6-x86_64.rpm -o splunk.rpm
 chmod 744 splunk.rpm
 yes | sudo rpm -i splunk.rpm
-sudo /opt/splunk/bin/splunk start
+sudo /opt/splunk/bin/splunk start --accept-license --answer-yes
