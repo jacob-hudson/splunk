1cb9441535e3999b9b3cc21a0b4fea2738415a4b
Jacob A. Hudson <jacob.alan.hudson@gmail.com>
2017-10-07 14:58:56 -0500

    updated for splunk 7.0.0

--- install_rpm.sh
+++ install_rpm.sh
@@ -4,8 +4,7 @@
 set -e -u
 
 cd /home/vagrant
-sudo yum -y install java git
-curl https://download.splunk.com/products/splunk/releases/6.6.3/linux/splunk-6.6.3-4b804538c686-linux-2.6-x86_64.rpm -o splunk.rpm
+curl -o splunk.rpm https://download.splunk.com/products/splunk/releases/7.0.0/linux/splunk-7.0.0-c8a78efdd40f-linux-2.6-x86_64.rpm
 chmod 744 splunk.rpm
 yes | sudo rpm -i splunk.rpm
 sudo /opt/splunk/bin/splunk start --accept-license --answer-yes
